ec83f815251535eebdf0f8f3f266770d533ea576
KennySmash <webb1911@gmail.com>
2024-08-25 02:41:19 +0100

    skills basic

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        <p>Skills</p>
+        <Skills
+            title="Languages"
+            :skillarr="lang"
+        />
     </div>
 </template>
+<script setup>
+import { ref } from 'vue'
+
+const lang = ref([
+    "Rust",
+    "Java",
+    "Python",
+    "Typescript",
+    "SQL",
+    "C#",
+])
+
+</script>
