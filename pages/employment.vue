80bac3887d9241f6ca843a4e91f7cd832e5a3c6c
Lucas Kimber <lucas.kimber@outlook.com>
2024-08-25 02:01:11 +0100

    Added Employment Timeline

--- pages/employment.vue
+++ pages/employment.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
         <p>Employment</p>
+        <Timeline 
+            pos="Teaching Assistant, Royal Holloway" 
+            location="Egham" 
+            start="Sep. 2023" 
+            end="Apr. 2024">Teaching C and Python programming courses</Timeline>
+        <Timeline 
+            pos="Software Engineer, Arqit" 
+            location="London" 
+            start="Jun. 2023" 
+            end="Sep. 2023">Used Cypress, Cucumber, and Gherkin to automate 
+            end-to-end integration tests.</Timeline>
+        <Timeline
+            pos="Embedded Software Engineer"
+            location="St. Neots"
+            start="Jun. 2022"
+            end="Jun. 2023">Worked on embedded measurement and analytics systems</Timeline> 
     </div>
 </template>
