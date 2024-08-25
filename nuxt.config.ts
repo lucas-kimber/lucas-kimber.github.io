0a2db1a2b58d44d62d6bc6de19bc8bfcd1d8e114
Lucas Kimber <lucas.kimber@outlook.com>
2024-08-25 03:15:01 +0100

    Added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-08-24"
+  compatibilityDate: "2024-08-24",
+  app: {
+    head: {
+      title: "Lucas Kimber",
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.ico' }
+      ]
+    }
+  },
 })
